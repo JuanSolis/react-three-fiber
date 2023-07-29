df2109b28cb81401a16477e22ea5ab2a6291ab2a
JuanSolis <juansolisgtsk@hotmail.com>
2023-07-29 14:07:11 -0600

    Creacion de figuras geometricas con frames y controles

--- src/components/Scene.jsx
+++ src/components/Scene.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useRef } from "react";
 import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 
 function Scene() {
   const mountRef = useRef(null);
@@ -16,12 +17,55 @@ function Scene() {
       1000
     );
 
+    camera.position.z = 12;
     scene.add(camera);
 
     // Renderer
     const renderer = new THREE.WebGLRenderer();
     renderer.setSize(currentMount.clientWidth, currentMount.clientHeight);
     currentMount.appendChild(renderer.domElement);
+
+    // Controls
+    const controls = new OrbitControls(camera, renderer.domElement);
+    controls.enableDamping = true;
+
+    // Cubo
+    const cube = new THREE.Mesh(
+      new THREE.BoxGeometry(1, 1, 1),
+      new THREE.MeshBasicMaterial()
+    );
+
+    scene.add(cube);
+
+    // Sphere
+    const geometrySphere = new THREE.SphereGeometry(0.8, 32, 16);
+    const materialSphere = new THREE.MeshBasicMaterial({ color: 0xffff00 });
+    const sphere = new THREE.Mesh(geometrySphere, materialSphere);
+    sphere.position.set(0, 1.5, 0);
+    scene.add(sphere);
+
+    // TorusKnot
+
+    const geometryTorusKnot = new THREE.TorusKnotGeometry(0.5, 0.18, 100, 16);
+    const materialTorusKnot = new THREE.MeshBasicMaterial({ color: 0xffff00 });
+    const torusKnot = new THREE.Mesh(geometryTorusKnot, materialTorusKnot);
+    torusKnot.position.set(0, -1.5, 0);
+    scene.add(torusKnot);
+
+    // Renderer the scene
+    const animate = () => {
+      controls.update();
+      renderer.render(scene, camera);
+      requestAnimationFrame(animate);
+    };
+
+    animate();
+
+    // Clean Up
+
+    return () => {
+      currentMount.removeChild(renderer.domElement);
+    };
   }, []);
 
   return (
