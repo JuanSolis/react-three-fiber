02e8d016ad0e4eb33c31204b76490cacb3e7d7c5
JuanSolis <juansolisgtsk@hotmail.com>
2023-07-29 18:53:47 -0600

    refactoring de template threejs en vite

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,13 @@ import { useState } from "react";
 import SceneWithGeometrys from "./components/SceneWithGeometrys";
 import SceneWithCube from "./components/SceneWithCube";
 import SceneWithModel from "./components/SceneWithModel";
+import Scene from "./components/Scene";
 import "./App.css";
 
 function App() {
   return (
     <>
-      <SceneWithModel />
+      <Scene />
     </>
   );
 }
